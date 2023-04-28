960ba86ee1ed71f85f96588c5192b78c4b9a0ce7
MicheleBellitti <michelebellitti272@gmail.com>
2023-04-28 17:20:57 +0200

    mpeg assignment added

---
+++ solutions/yuv4mpeg2/compile_yuv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: compile_y4m.sh [file_name]"
+  exit 1
+fi
+
+file_name=$1
+dir=`pwd`
+cd ./utils
+ls
+g++ -std=c++20 -c pgm.cpp -o pgm.o
+g++ -std=c++20 -c ppm.cpp -o ppm.o
+
+cd $dir
+
+g++ -std=c++20 -Wall $file_name.cpp utils/pgm.o utils/ppm.o -o ${file_name}.out
---
+++ solutions/yuv4mpeg2/y4m_grey.cpp
@@ -0,0 +1,102 @@
+#include "utils/pgm.h"
+#include "utils/ppm.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+#include <string.h>
+
+bool y4m_extract_grey(const std::string& filename, std::vector<mat<uint8_t> >& frames){
+    std::ifstream is(filename, std::ios::binary);
+    if(!is){
+        std::cerr << "Error opening the file: " << filename << std::endl;
+        return false;
+    }
+    std::string header;
+    is >> header;
+    if(strcmp(&header[0], "YUV4MPEG2") != 0)
+        return false;
+    int w, h;
+    std::string C, I, X,f, a;
+    for(size_t k=0;k<7;k++){
+        int tag = is.get();
+        switch (tag)
+        {
+        case 'W':
+            is >> w;
+            break;
+        case 'H':
+            is >> h;
+            break;
+        case 'C':
+            is >> C;
+            break;
+        case 'I':
+            is >> I;
+            break;
+        case 'F':
+            is >> f;
+            break;
+        case 'A':
+            is >> a;
+            break;
+        case 'X':
+            is >> X;
+            break;
+        
+        default:
+            break;
+        }
+    }
+    std::cout << "W: " << w << " " << " H: " << h << std::endl;
+    std::cout << "A: " << a << " " << " F: " << f << std::endl;
+    std::cout << "I: " << I << " " << " X: " << X << std::endl;
+    std::cout << "C: " << C << " " << std::endl;
+    while(true){
+        if(is.eof())
+            break;
+        std::string fr;
+        is.read(&fr[0],6);
+        std::string frame_i, frame_x;
+        for(size_t j=0;j<2;j++){
+            int frame_tag = is.get();
+            switch (frame_tag)
+            {
+            case 'I':
+                is >> frame_i;
+                break;
+            case 'X':
+                is >> frame_x;
+                break;
+            
+            default:
+                break;
+            }
+        }
+        mat<uint8_t> frame(w, h);
+        std::string data;
+        is >> data;
+        //std::cout << fr << std::endl;
+        //frames.push_back(frame);
+
+    }
+    
+    return false;
+
+   
+}
+
+int main(){
+    std::vector<mat<uint8_t> > frames;
+    if(y4m_extract_grey("utils/test1.y4m", frames)){
+        for(int i=0;i<frames.size();i++){
+            std::stringstream ss;
+            ss <<  std::setfill('0');
+            ss << "frame" << std::setw(3) << i << ".pgm";
+            save_pgm(ss.str(), frames[i]);
+        }
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+}
